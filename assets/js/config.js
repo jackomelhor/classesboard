9672efeac923e63721965085efeedc976e14d938
jackomelhor <106039304+jackomelhor@users.noreply.github.com>
2026-03-18 00:32:22 -0300

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.CLASSBOARD_CONFIG = {
   SUPABASE_URL: 'https://wtnvkvbotzijzwkssxga.supabase.co',
-  SUPABASE_ANON_KEY: 'COLE_AQsb_publishable_3jxlRnppR1mJBi4TaR4JTw_EI0_O6xH',
+  SUPABASE_ANON_KEY: 'sb_publishable_3jxlRnppR1mJBi4TaR4JTw_EI0_O6xH',
   APP_NAME: 'ClassBoard',
   APP_VERSION: 'v0.4',
 };
