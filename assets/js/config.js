82209d96593c416b2d35040caf04e37dd43929ca
jackomelhor <106039304+jackomelhor@users.noreply.github.com>
2026-03-18 09:42:19 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-window.CLASSBOARD_CONFIG = {
-  SUPABASE_URL: 'https://wtnvkvbotzijzwkssxga.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_3jxlRnppR1mJBi4TaR4JTw_EI0_O6xH',
-  APP_NAME: 'ClassBoard',
-  APP_VERSION: '1.0.0',
-};
